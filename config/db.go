9dbbf32c10824f1b801dcb0c37a771b89cfb4046
loggn <1137127358@qq.com>
2025-08-31 23:47:00 +0800

    添加数据库连接

--- config/db.go
+++ config/db.go
@@ -12,10 +12,11 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
+	// sslmode=disable 禁用ssl加密连接（仅开发测试环境可用，生产环境应更改） TimeZone=Asia/Shanghai 将数据库会话的时区设置为Asia/Shanghai（北京时间）
 	dsn := "host=localhost user=postgres password=123456 dbname=blogdb port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("failed to connect database: ", err)
+		log.Fatal("数据库连接失败: ", err)
 	}
 	DB = db
 	fmt.Println("数据库已连接")
